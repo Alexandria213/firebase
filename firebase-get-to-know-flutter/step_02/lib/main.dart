1c47b3566b601b8dd568e02c3a72e5da13b6d15c
Alexandria <alexandriacade0@gmail.com>
2022-11-15 21:06:51 -0600

    Step 5 Business logic
    import provider packages

--- firebase-get-to-know-flutter/step_02/lib/main.dart
+++ firebase-get-to-know-flutter/step_02/lib/main.dart
@@ -1,6 +1,14 @@
+import 'dart:async';                                      
+import 'package:firebase_auth/firebase_auth.dart'         
+    hide EmailAuthProvider, PhoneAuthProvider;            
+import 'package:firebase_core/firebase_core.dart';        
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';  
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';                  
 
+import 'firebase_options.dart';                           
+import 'src/authentication.dart';                         
 import 'src/widgets.dart';
 
 void main() {
